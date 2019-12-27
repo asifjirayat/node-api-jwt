2482db84f16e2c0e685a45183b8e8d8aea8387ca
asifjirayat <asifjirayat@gmail.com>
2019-12-27 16:52:50 +0530

    Route setuo

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 const environment = process.env.NODE_ENV;
 const stage = require('./config')[environment];
+const routes = require('./routes/index.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,10 +20,7 @@ if(environment !== 'production') {
   app.use(logger('dev'))
 }
 
-app.use('/api/v1', (req, res, next) => {
-  res.send('Hello');
-  next();
-});
+app.use('/api/v1', routes(router));
 
 app.listen(`${stage.port}`, () => {
   console.log(`Server now listening at localhost: ${stage.port}`);
