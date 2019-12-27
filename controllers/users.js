4b84c840e126fbf7d815f4e3f75a9714e81966cc
asifjirayat <asifjirayat@gmail.com>
2019-12-27 18:29:09 +0530

    Generate tokens using JWT

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
 const connUri = process.env.MONGO_LOCAL_CONN_URL;
 
 module.exports = {
   add: (req, res) => {
-    mongoose.connect(connUri, {userNewUrlParser: true}, (err) => {
+    mongoose.connect(connUri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true}, (err) => {
       let result = {};
       let status = 201;
       if(!err) {
@@ -28,6 +30,54 @@ module.exports = {
         result.status = status;
         result.result =   err;
         res.status(status).send(result);
+      }
+    });
+  },
+  login: (req, res) => {
+    const { name, password } = req.body;
+    mongoose.connect(connUri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true}, (err) => {
+      let result = {};
+      let status = 200;
+      if(!err) {
+        User.find({name: name}, (err, user) => {
+          if (!err && user) {
+            // We could compare passwords in our model instead of below
+            bcrypt.compare(password, user[0].password).then((match) => {
+              if (match) {
+                status = 200;
+                //create token
+                const payload = {user: user[0].name};
+                const options = {expiresIn: '2d', issuer: 'MJ'};
+                const secret = process.env.JWT_SECRET;
+                const token = jwt.sign(payload, secret, options);
+
+                result.token = token;
+                result.status = status;
+                result.result = user;
+              } else {
+                status = 401;
+                result.status = status;
+                result.error = 'Authentication error';
+              }
+              res.status(status).send(result);
+            }).catch((err) => {
+              status = 500;
+              result.status = status;
+              result.error = err;
+              res.status(status).send(result);
+            });
+          } else {
+            status = 404;
+            result.status = status;
+            result.error = err;
+            res.status(status).send(result);
+          }
+        });
+      } else {
+        // status = 500;
+        // result.status = status;
+        // result.error = err;
+        // res.status(status).send(result);
       }
     });
   }
